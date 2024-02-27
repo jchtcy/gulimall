db5d213f205df2bcc3d7ca5b7701dfb2817703b2
吉晨浩 <2547881414@qq.com>
2024-02-27 15:00:27 +0800

    项目初始结构创建

---
+++ gulimall-member/src/test/java/com/jch/gulimall/gulimallmember/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jch.gulimall.gulimallmember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
